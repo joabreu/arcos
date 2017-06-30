998105a7d1374597e7503033865ea31ecb1336f6
Jose Abreu <joabreu@synopsys.com>
2017-06-30 16:37:32 +0100

    arch: arc: mm: Remove debug calls in init function
    Remove some debug calls to memory allocation now that it
    works correctly.
    
    Signed-off-by: Jose Abreu <joabreu@synopsys.com>

--- arch/arc/mm/init.c
+++ arch/arc/mm/init.c
@@ -26,15 +26,10 @@ void setup_arch_memory(void)
 	end_pfn = PFN_DOWN(CONFIG_MEM_SIZE) + start_pfn;
 
 	init_bootmem(mapstart, start_pfn, end_pfn);
+
+	/* Mark kernel code as reserved */
 	bootmem_mark_reserved_region(start_pfn, mapstart - 1);
 
-	bootmem_zalloc(0x100);
-	bootmem_zalloc(0x123);
-	bootmem_zalloc(0x320);
-	bootmem_zalloc(0x400);
-	bootmem_zalloc(0x600);
-
-	printk("Memory @ %lx [%ldM]\n", __pa(PFN_PHYS(start_pfn)),
+	printk("Memory @ 0x%lx [%ldM]\n", __pa(PFN_PHYS(start_pfn)),
 			__pa(PFN_PHYS(end_pfn - start_pfn)) >> 20);
 }
-
